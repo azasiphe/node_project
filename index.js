2b226990ec90539b142c0288946ad7d2089650a2
azasiphe <azasiphendoro@gmail.com>
2024-02-23 15:50:53 +0200

    changes

--- index.js
+++ index.js
@@ -8,10 +8,7 @@ config();
 const PORT = process.env.PORT || 3500; 
 
 const app = express();
-app.use(cors({
-    origin: '*',
-    credentials:true
-  }));
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 app.use('/products', productsRouter);
