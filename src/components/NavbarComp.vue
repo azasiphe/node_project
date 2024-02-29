d11b232bd0c7d14cf67e08335f81137282b37e11
azasiphe <azasiphendoro@gmail.com>
2024-02-29 14:20:02 +0200

    commit

--- src/components/NavbarComp.vue
+++ src/components/NavbarComp.vue
@@ -1,56 +1,85 @@
 <template>
-  <nav class="navbar navbar-expand-lg text-white p-3">
-  <div class="container-fluid">
-    <ul class="navbar-nav gap-4">
-      <li class="nav-item">
-        <router-link to="/" class="navlink">Home</router-link> |
-      </li>
-      <li class="nav-item">
-        <router-link to="/about" class="navlink">About</router-link> |
-      </li>
-      <li class="nav-item">
-        <router-link to="/products" class="navlink">Products</router-link> |
-      </li>
-      <li class="nav-item">
-        <router-link to="/admin" class="navlink">Admin</router-link> |
-      </li>
-      <li class="nav-item">
-        <router-link to="/contact" class="navlink">Contact</router-link>
-      </li>
-    </ul>
+  <div>
+    <nav class="navbar navbar-expand-lg text-white p-3">
+      <div class="container-fluid">
+        <ul class="navbar-nav gap-4">
+          <li class="nav-item">
+            <router-link to="/" class="navlink">Home</router-link> |
+          </li>
+          <li class="nav-item">
+            <router-link to="/about" class="navlink">About</router-link> |
+          </li>
+          <li class="nav-item">
+            <router-link to="/products" class="navlink">Products</router-link> |
+          </li>
+          <li class="nav-item">
+            <router-link to="/admin" class="navlink">Admin</router-link> |
+          </li>
+          <li class="nav-item">
+            <router-link to="/contact" class="navlink">Contact</router-link>
+          </li>
+        </ul>
+        
+        <a class="navbar-brand" href="#">Navbar</a>
+        
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <button @click="show" class="list">Price list</button>
+            <a class="nav-link" href="#">Register</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="price-list-container">
     
-    <a class="navbar-brand" href="#">Navbar</a>
-    
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Register</a>
-      </li>
-    </ul>
-</div>
-</nav>
+     
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  components:{
-
-  }
+  data() {
+    return {
+      showImage: false
+    };
+  },
+  show() {
+      
+      window.open('https://i.ibb.co/B6kNgqw/Black-And-Yellow-Minimalist-Photography-Pricelist.png', '_blank');
+    }
+  
 }
 </script>
 
 <style scoped>
-nav{
+nav {
   background-color: #1A1E25;
 }
-.navlink{
+
+.navlink {
   color: white;
 }
-.nav-link{
+
+.list {
+  background-color: purple;
+}
+
+.list:hover {
+  background-color: yellow;
+}
+
+.nav-link {
   color: yellow;
 }
-/* footer p{
-  padding: 40px 0;
-  text-align: center;
-} */
 
+.price-list-container {
+  margin-top: 20px;
+  text-align: center;
+}
+
+.price-list-image {
+  margin-top: 10px;
+}
 </style>
